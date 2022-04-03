a16ad949f91345e13046f851ed149bce4e85816f
Clarence Siew <clarence.siew@gmail.com>
2022-04-03 16:53:24 +1000

    Fix ts-starter source dir reference error

--- create.sh
+++ create.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
+echo "Number of arguments provided: $#"
+
+if (( $# -ge 1 )); then
   echo "You must provide the target path"
   exit 1
 fi
@@ -20,7 +22,7 @@ case $project_type in
     ;;
 esac
 
-ts_starter_src=$(pwd)
+ts_starter_src="$(pwd)/ts-starter"
 
 echo "ts_starter location: ${ts_starter_src}"
 
@@ -32,36 +34,37 @@ else
 fi
 
 if [ -d "$target_path" ]; then
-  cd $target_path
-  target_path_full=$(pwd)
-  echo "Target location: ${target_path_full}"
-
-  echo "Installing base dev dependencies..."
-  npm init -y
-  npm list -g | grep typescript || npm i -g typescript
-  npm i -D ts-node ts-node-dev eslint @types/node @types/validator
-
-  if [ "$project_type" -eq "api" ]; then
-    echo "Installing API dev dependencies..."
-    npm i express sequelize pg pg-hstore bcrypt dotenv
-    npm i -D @types/express @types/sequelize @types/bcrypt jsonwebtoken @types/jsonwebtoken
-  fi
-  
-  echo "Copying config files..."
-  cp $ts_starter_src/tsconfig.json $target_path_full/tsconfig.json
-  cp $ts_starter_src/.eslintrc $target_path_full/.eslintrc
-  cp $ts_starter_src/.eslintignore $target_path_full/.eslintignore
-
-  echo "Updating package.json scripts..."
-  node $ts_starter_src/update_package_json.js
-  
-  echo "Done"
-  exit 0
+  echo "Target directory already exists: ${target_path}"
+  exit 1
 else
-  echo "Target directory does not exist: ${target_path}"
+  mkdir "$target_path"
 fi
 
+cd $target_path
+target_path_full=$(pwd)
+echo "Target location: ${target_path_full}"
+
+echo "Installing base dev dependencies..."
+npm init -y
+if [ `npm list -g | grep -c typescript` -eq 0 ]; then
+  echo "It looks like you do not have TypeScript installed!"
+  echo "Please install using: npm i -g typescript"
+fi
+npm i -D ts-node ts-node-dev eslint @types/node @types/validator
+
+if [ "$project_type" = "api" ]; then
+  echo "Installing API dev dependencies..."
+  npm i express sequelize pg pg-hstore bcrypt dotenv
+  npm i -D @types/express @types/sequelize @types/bcrypt jsonwebtoken @types/jsonwebtoken
+fi
+
+echo "Copying config files..."
+cp $ts_starter_src/tsconfig.json $target_path_full/tsconfig.json
+cp $ts_starter_src/.eslintrc $target_path_full/.eslintrc
+cp $ts_starter_src/.eslintignore $target_path_full/.eslintignore
+
+echo "Updating package.json scripts..."
+node $ts_starter_src/update_package_json.js
+
 echo "Done"
-
-exit 1
-
+exit 0
